a78975417f5692f62bcd35f6b459ce599cd23202
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2020-12-11 20:52:03 +0800

    Challenge 1 of 50 Finished widget Using React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { useReducer, createContext, useContext } from "react";
 import "./styles.css";
-//import Panel from "./components/Panel";
 
 const activeState = [
   {
     season: "Spring",
-    active: false,
+    toggle: false,
     img:
       "https://images.unsplash.com/photo-1493589976221-c2357c31ad77?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
   },
   {
     season: "Summer",
-    active: false,
+    toggle: false,
     img:
       "https://images.unsplash.com/photo-1586500036065-bdaeac7a4feb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
   },
   {
     season: "Fall",
-    active: false,
+    toggle: false,
     img:
       "https://images.unsplash.com/photo-1508255139162-e1f7b7288ab7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
   },
   {
     season: "Winter",
-    active: false,
+    toggle: false,
     img:
       "https://images.unsplash.com/photo-1551582045-6ec9c11d8697?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
   }
@@ -32,7 +31,15 @@ const activeState = [
 const activeReducer = (state, action) => {
   switch (action.type) {
     case "TOGGLE":
-      return !action.payload;
+      const falseAll = state.map((x) => Object.assign(x, { toggle: false }));
+      const toggled = Object.assign(falseAll[action.payload], {
+        toggle: true
+      });
+      const newState = state.map((x, index) =>
+        index !== action.payload ? x : toggled
+      );
+      console.log(newState);
+      return newState;
     default:
       return state;
   }
@@ -44,14 +51,14 @@ export default function App() {
   const [state, dispatch] = useReducer(activeReducer, activeState);
 
   return (
-    <Context.Provider value={dispatch}>
+    <Context.Provider value={{ dispatch }}>
       <div className="container">
         {state.map((item, index) => (
           <Panels
+            id={index}
             title={item.season}
             img={item.img}
-            id={index}
-            stat={item.active}
+            stat={item.toggle}
           />
         ))}
       </div>
@@ -59,12 +66,16 @@ export default function App() {
   );
 }
 
-const Panels = ({ img, title, stat, id }) => {
-  const dispatch = useContext(Context);
+const Panels = ({ img, title, id, stat }) => {
+  const context = useContext(Context);
+  const { dispatch } = context;
+  const clickHandler = (id) => {
+    dispatch({ type: "TOGGLE", payload: id });
+  };
+
   return (
     <div
-      key={id}
-      onClick={() => dispatch({ type: "TOGGLE", payload: stat })}
+      onClick={() => clickHandler(id)}
       className={stat ? "panel active" : "panel"}
       style={{ backgroundImage: `url(${img})` }}
     >
